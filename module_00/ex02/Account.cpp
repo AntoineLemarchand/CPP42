6030b1aed40263146f60489f83db247326f29359
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-04-11 16:36:21 +0200

    start 0/02

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <ctime>
+
+int	Account::_NbAccounts = 0;
+
 static int	Account::getNbAccounts( void )
 {}
 
@@ -14,10 +19,22 @@ static void	Account::displayAccountsInfos( void )
 {}
 
 Account::Account( int initial_deposit )
-{}
+{
+	this->_totalAmount = initial_deposit;
+	this->_displayTimestamp();
+	std::cout << " index:" << this->_NbAccounts;
+	std::cout << ";amount:" << this.initial_deposit;
+	std::cout << ";created" << std::endl;
+	this->NbAccounts++;
+}
 
 Account::~Account( void )
-{}
+{
+	this->_displayTimestamp();
+	std::cout << " index:" << this->_NbAccounts;
+	std::cout << ";amount:" << this.initial_deposit;
+	std::cout << ";closed" << std::endl;
+}
 
 void	Account::makeDeposit( int deposit )
 {}
@@ -30,3 +47,14 @@ int		Account::checkAmount( void ) const
 
 void	Account::displayStatus( void ) const
 {}
+
+static void	Account::_displayTimestamp( void )
+{
+	struct tm	*local;
+	char		ts[19] = {0};
+
+	std::timet timer = std::time(NULL);
+	local = localtime(&timer);
+	std::strftime(timestamp, 19, "[%Y%m%d%H%M%S]", local);
+	std::cout << timestamp;
+}
