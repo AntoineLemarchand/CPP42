eb4d23eaa0c7debf83243b567177dd676612c367
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-04-13 14:58:04 +0200

    module00 done

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -1,68 +1,96 @@
 #include <iostream>
 #include <ctime>
+#include "Account.hpp"
 
-static int	Account::_nbAccounts = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
-static int	Account::getNbAccounts( void )
+int	Account::getNbAccounts( void )
 {
-	return (this->_accountIndex)
+	return (_nbAccounts);
 }
 
-static int	Account::getTotalAmount( void )
+int	Account::getTotalAmount( void )
 {
-	return (this->_totalAmount);
+	return (_totalAmount);
 }
 
-static int	Account::getNbDeposits( void )
+int	Account::getNbDeposits( void )
 {
-	return (this->_nbDeposits);
+	return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals( void )
+int	Account::getNbWithdrawals( void )
 {
-	return (this->_nbWithdrawals);
+	return (_totalNbWithdrawals);
 }
 
-static void	Account::displayAccountsInfos( void )
+void	Account::displayAccountsInfos( void )
 {
-	this->_displayTimestamp();
-	std::cout << " index:" << this->_NbAccounts;
-	std::cout << ";amount:" << this->_totalAmount;
-	std::cout << ";deposits:" << this->_nbDeposits;
-	std::cout << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+	_displayTimestamp();
+	std::cout << " accounts:" << _nbAccounts;
+	std::cout << ";total:" << _totalAmount;
+	std::cout << ";deposits:" << _totalNbDeposits;
+	std::cout << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 Account::Account( int initial_deposit )
 {
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
-	this->_totalAmount = initial_deposit;
+	this->_amount = initial_deposit;
+	this->_totalAmount += initial_deposit;
+	this->_accountIndex = this->_nbAccounts;
+	this->_nbAccounts++;
 	this->_displayTimestamp();
-	std::cout << " index:" << this->_NbAccounts;
-	std::cout << ";amount:" << this->_totalAmount;
+	std::cout << " index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
 	std::cout << ";created" << std::endl;
-	this->_accountIndex = this->NbAccounts;
-	this->NbAccounts++;
 }
 
 Account::~Account( void )
 {
 	this->_displayTimestamp();
 	std::cout << " index:" << this->_accountIndex;
-	std::cout << ";amount:" << this->_totalAmount;
+	std::cout << ";amount:" << this->_amount;
 	std::cout << ";closed" << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";p_amount:" << _amount;
+	std::cout << ";deposit:" << deposit;
 	this->_totalAmount += deposit;
+	this->_amount += deposit;
 	this->_nbDeposits++;
+	this->_totalNbDeposits++;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex;
+	std::cout << ";p_amount:" << this->_amount;
+	std::cout << ";withdrawal:";
+	if (this->_amount <= withdrawal)
+	{
+		std::cout << "refused" << std::endl;
+		return (1);
+	}
+	this->_amount -= withdrawal;
 	this->_totalAmount -= withdrawal;
 	this->_nbWithdrawals++;
+	this->_totalNbWithdrawals++;
+	std::cout << withdrawal;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	return (0);
 }
 
 int		Account::checkAmount( void ) const
@@ -73,19 +101,19 @@ int		Account::checkAmount( void ) const
 void	Account::displayStatus( void ) const
 {
 	this->_displayTimestamp();
-	std::cout << " accounts:" << this->_NbAccounts;
-	std::cout << ";total:" << this->_totalAmount;
+	std::cout << " index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
 	std::cout << ";deposits:" << this->_nbDeposits;
 	std::cout << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
-static void	Account::_displayTimestamp( void )
+void	Account::_displayTimestamp( void )
 {
 	struct tm	*local;
 	char		ts[19] = {0};
 
-	std::timet timer = std::time(NULL);
+	std::time_t timer = std::time(NULL);
 	local = localtime(&timer);
-	std::strftime(timestamp, 19, "[%Y%m%d%H%M%S]", local);
-	std::cout << timestamp;
+	std::strftime(ts, 19, "[%Y%m%d_%H%M%S]", local);
+	std::cout << ts;
 }
