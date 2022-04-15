a7f548c517dd127be9bcf27b4ebf67de97b4b681
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-04-15 12:15:35 +0200

    01/06

---
+++ module_01/ex06/Harl.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "Harl.hpp"
+
+void Harl::complain( std::string level )
+{
+	void	(Harl::*fct[])( void ) = 
+	{
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	std::string inputs[] = 
+	{
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+
+	bool instr = false;
+	for (int i = 0; i < 4; i++)
+		if (!level.compare(inputs[i]) || instr)
+		{
+			instr = true;
+			void (Harl::*select)( void ) = fct[i];
+			std::cout << "[" << inputs[i] << "]" << std::endl;
+			(this->*select)();
+			std::cout << std::endl;
+		}
+	if (!instr)
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
+
+void Harl::debug( void )
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error( void )
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
