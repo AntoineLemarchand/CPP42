267b11819d3b97ddafcb5df81e73f6609e5f8673
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-04-18 11:00:26 +0200

    02/02

---
+++ module_02/ex02/includes/Fixed.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed( const Fixed& fixed );
+		Fixed( const int val );
+		Fixed( const float val );
+
+		~Fixed();
+
+		Fixed	&operator = ( const Fixed& fixed );
+		friend std::ostream	&operator << ( std::ostream& out, const Fixed& fixed);
+
+		bool	operator > ( const Fixed& fixed ) const;
+		bool	operator < ( const Fixed& fixed ) const;
+		bool	operator >= ( const Fixed& fixed ) const;
+		bool	operator <= ( const Fixed& fixed ) const;
+		bool	operator == ( const Fixed& fixed ) const;
+		bool	operator != ( const Fixed& fixed ) const;
+
+		Fixed	operator + ( const Fixed& fixed ) const;
+		Fixed	operator - ( const Fixed& fixed ) const;
+		Fixed	operator * ( const Fixed& fixed ) const;
+		Fixed	operator / ( const Fixed& fixed ) const;
+
+		Fixed	&operator ++ ( void );
+		Fixed	&operator -- ( void );
+		Fixed	operator ++ ( int );
+		Fixed	operator -- ( int );
+
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+
+		static Fixed&	min(Fixed& a, Fixed& b);
+		static Fixed&	max(Fixed& a, Fixed& b);
+		static const Fixed	min(const Fixed& a, const Fixed& b);
+		static const Fixed	max(const Fixed& a, const Fixed& b);
+	private:
+		int					_value;
+		static const int	_scaling_factor = 8;
+};
---
+++ module_02/ex02/srcs/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c = Fixed(10);
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+	std::cout << "ADDITIONAL TEST" << std::endl;
+
+	std::cout << Fixed::min( a, b ) << std::endl;
+
+	std::cout << c << std::endl;
+	std::cout << --c << std::endl;
+	std::cout << c << std::endl;
+	std::cout << c-- << std::endl;
+	std::cout << c << std::endl;
+
+
+	return 0;
+}
