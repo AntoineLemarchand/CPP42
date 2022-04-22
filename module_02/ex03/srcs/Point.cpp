20e18f3b86220e9112961b3fa45e3e30bf257d4f
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-04-22 10:43:06 +0200

    refactored 02

--- module_02/ex03/srcs/Point.cpp
+++ module_02/ex03/srcs/Point.cpp
@@ -4,8 +4,7 @@ Point::Point()
 {}
 
 Point::Point(const float x, const float y) : _x(Fixed(x)), _y(Fixed(y))
-{
-}
+{}
 
 Point::Point(const Point& point) : _x(point.getX()), _y(point.getY())
 {
@@ -13,7 +12,8 @@ Point::Point(const Point& point) : _x(point.getX()), _y(point.getY())
 
 Point& Point::operator = (const Point& point)
 {
-	*this = Point(point);
+	(void)point;
+	std::cerr << "Point cannot be overloaded as its values are const" << std::endl;
 	return (*this);
 }
 
